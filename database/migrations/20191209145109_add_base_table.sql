e6b6a28da3cef91db0962752a996a90be4f21a35
Camilo Acuña <camilo.aku@gmail.com>
2019-12-27 00:35:30 -0300

    add migrations files

---
+++ database/migrations/20191209145109_add_base_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+  id uuid primary key default gen_random_uuid() UNIQUE,
+  email varchar(255)  NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  created_at timestamptz default now(),
+  updated_at timestamptz default now(),
+  deleted_at timestamptz
+);
+
+create trigger update_users_update_at
+before update on users for each row execute procedure update_updated_at_column();
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
